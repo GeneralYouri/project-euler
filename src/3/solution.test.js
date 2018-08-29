62a3e8c142c3b37695204986edf6308b6848d54e
Youri <youribok@msn.com>
2018-08-29 05:13:00 +0200

    Adds custom tests to problem 3

--- src/3/solution.test.js
+++ src/3/solution.test.js
@@ -4,6 +4,13 @@ test('Provided test cases', () => {
     expect(solution('13195')).toBe(29);
 });
 
+test('Custom test cases', () => {
+    expect(solution('2')).toBe(2);
+    expect(solution('3')).toBe(3);
+    expect(solution('10')).toBe(5);
+    expect(solution('8008135')).toBe(1601627);
+});
+
 test('Problem input', () => {
     expect(solution(defaultInput)).toBe(6857);
 });
