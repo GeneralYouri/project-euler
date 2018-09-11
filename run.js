a46ba6e18a46947d23a9ea630262f7e244e3df39
Youri <youribok@msn.com>
2018-09-11 04:15:27 +0200

    Fixes some minor CLI issues

--- run.js
+++ run.js
@@ -27,17 +27,17 @@ const solutions = require('./src');
 
 const formatTime = ms => Number(ms.toPrecision(7)).toFixed(3);
 
-const formatHeader = problem => problem.padEnd(3, ' ');
+const formatHeader = problem => problem.toString().padStart(5, ' ');
 
-const formatInfo = ([year], time, answer) => {
-    const header = formatHeader(year);
+const formatInfo = ([problem], time, answer) => {
+    const header = formatHeader(problem);
     const timeStr = formatTime(time).slice(0, 8).padStart(8, ' ');
-    return `${header} ✅ | \x1b[38;5;240mTime:\x1b[0m ${timeStr} ms | \x1b[38;5;240mAnswer:\x1b[0m ${answer}`;
+    return `${header} | \x1b[38;5;240mTime:\x1b[0m ${timeStr} ms | \x1b[38;5;240mAnswer:\x1b[0m ${answer}`;
 };
 
 const formatError = ([problem], message) => {
     const header = formatHeader(problem);
-    return `\x1b[31m${header}\x1b[0m ❌ | \x1b[31mSkip:\x1b[0m             | \x1b[31m${message}\x1b[0m`;
+    return `\x1b[31m${header}\x1b[0m | \x1b[31mSkip:\x1b[0m             | \x1b[31m${message}\x1b[0m`;
 };
 
 const formatTotal = (problem, time, solved) => {
@@ -53,7 +53,7 @@ const runSolution = (problem, fn, input) => {
         return false;
     }
 
-    // Temporary suppress console.log
+    // Temporarily suppress console.log
     const oldLogger = console.log;
     console.log = () => {
     };
@@ -89,7 +89,6 @@ problems.sort((a, b) => a - b).forEach((problem) => {
     const solution = solutions[problem];
     if (!solution) {
         console.log(formatError([problem], 'No solution yet'));
-        console.log('');
         return;
     }
 
