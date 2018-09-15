fbe829cc67451b7e211920cc6c4b25a6f336bd3c
Youri <youribok@msn.com>
2018-09-16 01:20:57 +0200

    Improves formatting slightly for running individual problem solutions

--- lib.js
+++ lib.js
@@ -25,8 +25,8 @@ const runSolution = (name, fn, input) => {
     const answer = fn(...input);
     const time = process.hrtime(start);
     console.log('-----', name, '-----');
-    console.log('Problem answer:', answer);
-    console.log('Problem time: %d ms', time[0] * 1000 + time[1] / 1000000);
+    console.log('\x1b[38;5;240mAnswer:\x1b[0m', answer);
+    console.log('\x1b[38;5;240mTime:  \x1b[0m', time[0] * 1000 + time[1] / 1000000, 'ms');
     console.log('-------------------------');
 };
 
